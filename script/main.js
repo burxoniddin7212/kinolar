9c89b1a8be11c18794bddc900d9e024e93705ae0
burxoniddin7212 <liverpul7212@gmail.com>
2022-05-17 00:49:31 +0500

    qidirish

--- script/main.js
+++ script/main.js
@@ -1,4 +1,27 @@
 let elPokimonsList = $(".js-list");
+let elFindNameFilm = $(".js-film-name-input");
+let elRezultFind = $(".js-result-fil-name");
+let elForm = $(".js-form");
+
+let elList = document.createElement("ul");
+let newLiTemplate = document.querySelector(".item").content;
+let elNewLi = newLiTemplate.cloneNode(true);
+
+
+
+
+elForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  let findFilmIndex = kinolar.findIndex((name) => {
+    return name.title == elFindNameFilm.value;
+  })
+  console.log(findFilmIndex);
+  elNewLi.querySelector(".heading").textContent = kinolar[findFilmIndex].title.toString();
+  elNewLi.querySelector(".film-genres").textContent = kinolar[findFilmIndex].genres.toString();
+  elNewLi.querySelector(".film-year").textContent = kinolar[findFilmIndex].year.toString();
+  elList.append(elNewLi);
+  elRezultFind.append(elList);
+})
 
 let creatItem = function(kino){
   let newLi  = document.createElement("li");
@@ -23,5 +46,4 @@ kinolar.forEach(function(kino){
   fragment.append(creatItem(kino));
 })
 
-elPokimonsList.append(fragment);
-console.log(elPokimonsList);
+//elPokimonsList.append(fragment);
