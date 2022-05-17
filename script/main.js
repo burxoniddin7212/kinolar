efe0581f04d0c4e45ac1d821a9a09ab1a46f1d25
burxoniddin7212 <liverpul7212@gmail.com>
2022-05-17 11:05:54 +0500

    yoq kino bosa yoq diydi

--- script/main.js
+++ script/main.js
@@ -2,41 +2,47 @@ let elPokimonsList = $(".js-list");
 let elFindNameFilm = $(".js-film-name-input");
 let elRezultFind = $(".js-result-fil-name");
 let elForm = $(".js-form");
+let newLiTemplate = document.querySelector("#item").content;
+
 
 let elList = document.createElement("ul");
 
+let a = [];
 
 elList.classList.add("list-unstyled");
 
 elForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
 
+  let b = 0;
   elList.innerHTML = "";
   
+
+  
+
   let findFilmIndex = kinolar.findIndex((name) => {
     if(name.title == elFindNameFilm.value){
     return name.title == elFindNameFilm.value;
-    }
-    else{
-      return undefined;
     }
   })
 
   console.log(findFilmIndex);
   
-  if(findFilmIndex != null){
-    let newLiTemplate = document.querySelector(".item").content;
+  if(findFilmIndex >= 0){
     let elNewLi = newLiTemplate.cloneNode(true);
     elNewLi.querySelector(".heading").textContent = kinolar[findFilmIndex].title.toString();
     elNewLi.querySelector(".film-genres").textContent = kinolar[findFilmIndex].genres.toString();
     elNewLi.querySelector(".film-year").textContent = kinolar[findFilmIndex].year.toString();
     elList.append(elNewLi);
     elRezultFind.append(elList);
+    b++;
   }
-  else if(findFilmIndex == undefined){
-    let newLiTemplate = document.querySelector(".item").content;
+  if(b == 0){
+    let newLiTemplate = document.querySelector("#item").content;
     let elNewLi = newLiTemplate.cloneNode(true);
     elNewLi.querySelector(".heading").textContent = "Bunday kino yo'q";
+    elList.append(elNewLi);
+    elRezultFind.append(elList);
   }
 
 })
