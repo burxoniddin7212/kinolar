3a2ded45e8b799d3892239fbbe8d095aa2fe395a
burxoniddin7212 <liverpul7212@gmail.com>
2022-05-17 08:25:37 +0500

    yahshiroq boldi

--- script/main.js
+++ script/main.js
@@ -4,23 +4,41 @@ let elRezultFind = $(".js-result-fil-name");
 let elForm = $(".js-form");
 
 let elList = document.createElement("ul");
-let newLiTemplate = document.querySelector(".item").content;
-let elNewLi = newLiTemplate.cloneNode(true);
 
 
-
+elList.classList.add("list-unstyled");
 
 elForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
+
+  elList.innerHTML = "";
+  
   let findFilmIndex = kinolar.findIndex((name) => {
+    if(name.title == elFindNameFilm.value){
     return name.title == elFindNameFilm.value;
+    }
+    else{
+      return undefined;
+    }
   })
+
   console.log(findFilmIndex);
-  elNewLi.querySelector(".heading").textContent = kinolar[findFilmIndex].title.toString();
-  elNewLi.querySelector(".film-genres").textContent = kinolar[findFilmIndex].genres.toString();
-  elNewLi.querySelector(".film-year").textContent = kinolar[findFilmIndex].year.toString();
-  elList.append(elNewLi);
-  elRezultFind.append(elList);
+  
+  if(findFilmIndex != null){
+    let newLiTemplate = document.querySelector(".item").content;
+    let elNewLi = newLiTemplate.cloneNode(true);
+    elNewLi.querySelector(".heading").textContent = kinolar[findFilmIndex].title.toString();
+    elNewLi.querySelector(".film-genres").textContent = kinolar[findFilmIndex].genres.toString();
+    elNewLi.querySelector(".film-year").textContent = kinolar[findFilmIndex].year.toString();
+    elList.append(elNewLi);
+    elRezultFind.append(elList);
+  }
+  else if(findFilmIndex == undefined){
+    let newLiTemplate = document.querySelector(".item").content;
+    let elNewLi = newLiTemplate.cloneNode(true);
+    elNewLi.querySelector(".heading").textContent = "Bunday kino yo'q";
+  }
+
 })
 
 let creatItem = function(kino){
@@ -46,4 +64,4 @@ kinolar.forEach(function(kino){
   fragment.append(creatItem(kino));
 })
 
-//elPokimonsList.append(fragment);
+elPokimonsList.append(fragment);
